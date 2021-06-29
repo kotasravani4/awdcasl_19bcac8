ee2ff048a98695361bf3ff3c33e4abee63215926
kotasravani4 <71435062+kotasravani4@users.noreply.github.com>
2021-06-29 11:40:27 +0530

    Swap.java

---
+++ Swap.java
@@ -0,0 +1,12 @@
+import java.utill.*;
+class Swap
+{
+int a,b;
+Scanner sc=new Scanner(system.in)
+System.out.println("enter a and b values");
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println("After swapping a and b values are"+a"\n"+b);
+}
+} 
